2d2a22a7b2ffaf6dc988fa7a9d5b6333d19dd5d7
deltora72 <mohammad.j.dim@gmail.com>
2019-03-28 17:36:09 +0200

    Update

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+// Destructuring
+// let med = [1,2,3];
+//
+// let [m, ...n] = med;
+//
+// console.log(m, n);
+
+axios.get('https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+});
